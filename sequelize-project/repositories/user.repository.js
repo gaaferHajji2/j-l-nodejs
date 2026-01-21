52018666fc4546f766cde14010638614626901ff
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-21 12:24:02 +0300

    Create user repository

---
+++ sequelize-project/repositories/user.repository.js
@@ -0,0 +1,80 @@
+const BaseRepository = require('./base.repository');
+
+class UserRepository extends BaseRepository {
+  constructor() {
+    super(require('../models').User);
+  }
+
+  // Get all users with only specific columns for listing
+  async findAllUsers(includeAssociations = false) {
+    const options = {
+      attributes: ['id', 'username', 'email', 'createdAt'], // Only necessary columns
+      order: [['createdAt', 'DESC']]
+    };
+
+    if (includeAssociations) {
+      options.include = [
+        {
+          model: require('../models').Profile,
+          as: 'profile',
+          attributes: ['firstName', 'lastName'] // Only specific columns from profile
+        },
+        {
+          model: require('../models').Post,
+          as: 'posts',
+          attributes: ['id', 'title', 'isPublished'], // Only specific columns from posts
+          required: false // LEFT JOIN instead of INNER JOIN
+        }
+      ];
+    }
+
+    return await this.findAll(options);
+  }
+
+  // Get user by ID with all detailed information
+  async findUserById(id) {
+    return await this.findById(id, {
+      include: [
+        {
+          model: require('../models').Profile,
+          as: 'profile'
+          // All columns from profile (no attributes specified)
+        },
+        {
+          model: require('../models').Post,
+          as: 'posts',
+        //   include: [
+        //     {
+        //       model: require('../models').Tag,
+        //       as: 'tags',
+        //       through: { attributes: [] } // Hide junction table attributes
+        //     }
+        //   ]
+        }
+      ]
+    });
+  }
+
+  async findUserWithPosts(id) {
+    return await this.findById(id, {
+      attributes: ['id', 'username', 'email'],
+      include: [
+        {
+          model: require('../models').Post,
+          as: 'posts',
+          attributes: ['id', 'title', 'createdAt'],
+          where: { isPublished: true }
+        }
+      ]
+    });
+  }
+
+  async findUsersByEmail(email) {
+    return await this.findAll({
+      where: { email },
+      attributes: ['id', 'username', 'email']
+    });
+  }
+}
+
+module.exports = new UserRepository();
