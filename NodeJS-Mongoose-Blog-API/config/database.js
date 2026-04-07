30e2c2f0e9a5416c96cad75abde560f1dc930424
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-07 09:47:02 +0300

    Install env package && finish from connect to db

---
+++ NodeJS-Mongoose-Blog-API/config/database.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        const conn =  await mongoose.connect(process.env.MONGOOSE_URI || 'mongodb://localhost:27017/blog_db')
+
+        console.log(`The connection host is: ${conn.connection.host}`)
+
+        mongoose.connection.on('error', (err) => {
+            console.error(`Connection error: ${err.message}`)
+        })
+
+        mongoose.connection.on('disconnected', (err) => {
+            console.log('Disconnected')
+        })
+    } catch (error) {
+        console.error(`error is: ${error.message}`)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
