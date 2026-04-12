5a4938e23ed17334a420feaa34809bbd864fec96
JafarLoka <gaafer.hajji1995@gmail.com>
2026-04-12 10:45:50 +0300

    Build server.js

---
+++ NodeJS-Mongoose-Blog-API/server.js
@@ -0,0 +1,21 @@
+// server.js
+require('dotenv').config();
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err.message);
+  server.close(() => process.exit(1));
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err.message);
+  process.exit(1);
+});
