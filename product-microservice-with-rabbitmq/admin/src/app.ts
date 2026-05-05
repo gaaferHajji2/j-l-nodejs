c5f69a515fef3342c0b762c1df20c64e18364111
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-05 11:38:58 +0300

    Adding json middleware for express app

--- product-microservice-with-rabbitmq/admin/src/app.ts
+++ product-microservice-with-rabbitmq/admin/src/app.ts
@@ -12,9 +12,12 @@ console.log(`Path is: ${path}`)
 
 dotenv.config({ path })
 const app = express()
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
-}))
+app.use(
+    cors({
+        origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+    })
+)
+app.use(express.json())
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on: http://localhost:${process.env.PORT}`)
