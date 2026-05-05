9a0cb430972c18b715a452aa4ca4f49475306327
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-05 11:31:58 +0300

    Add the dotenv for the project

--- product-microservice-with-rabbitmq/admin/src/app.ts
+++ product-microservice-with-rabbitmq/admin/src/app.ts
@@ -1,5 +1,21 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const path = __dirname+'/../.env'
+console.log(`Path is: ${path}`)
+
+dotenv.config({ path })
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+}))
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on: http://localhost:${process.env.PORT}`)
+})
