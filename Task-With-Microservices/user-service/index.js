c6bc93037779c53e1e4da3ffd44a9f2df719f7f5
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-18 09:55:22 +0300

    Adding the dotenv package and use it

--- Task-With-Microservices/user-service/index.js
+++ Task-With-Microservices/user-service/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 let app = express()
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, }))
 
@@ -10,10 +12,11 @@ app.get('/', (req, res) => {
         msg: "Index route"
     })
 })
+let PORT = process.env.PORT || 3000
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
     if(err) {
         console.log("Error in creating server: ", error)
     }
-    console.log(`Server running on: http://localhost:3000/`)
+    console.log(`Server running on: http://localhost:${PORT}/`)
 })
