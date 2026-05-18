8d56500e79eeae6233272c353bc16c7092731f1d
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-18 09:49:51 +0300

    Adding the required middlewares and fix a bug

--- Task-With-Microservices/user-service/index.js
+++ Task-With-Microservices/user-service/index.js
@@ -1,6 +1,9 @@
-import Express from 'express'
+import express from 'express'
 
-app = Express()
+let app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true, }))
 
 app.get('/', (req, res) => {
     return res.json({
