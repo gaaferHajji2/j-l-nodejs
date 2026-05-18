7d7a8f999636725d11c492f82a41132ea4234374
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-18 10:01:36 +0300

    Adding connection to MongoDB

--- Task-With-Microservices/user-service/index.js
+++ Task-With-Microservices/user-service/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -7,13 +8,20 @@ let app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, }))
 
+let main = async () => {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log(`Connect to Mongodb successfully`)
+}
+
+main().catch((err) => console.log(`Error in connecting to MongoDB`, err))
+
 app.get('/', (req, res) => {
     return res.json({
         msg: "Index route"
     })
 })
-let PORT = process.env.PORT || 3000
 
+let PORT = process.env.PORT || 3000
 app.listen(PORT, (err) => {
     if(err) {
         console.log("Error in creating server: ", error)
