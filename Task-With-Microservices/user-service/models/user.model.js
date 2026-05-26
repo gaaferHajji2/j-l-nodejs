77813519e47f403d11e153f02c52264bb47646de
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-05-26 09:57:10 +0300

    Correct the email validation regular expression

--- Task-With-Microservices/user-service/models/user.model.js
+++ Task-With-Microservices/user-service/models/user.model.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         trim: true,
         match: [
-            /^[a-zA-Z0-9._%+-]+\+[0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
             'Please add a valid email',
         ],
     },
