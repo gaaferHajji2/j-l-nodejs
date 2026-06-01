4af51aa66ab93d67a4369a5903c4be4d76d14e1e
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-06-01 12:12:53 +0300

    Adding the task route without testing

--- Task-With-Microservices/task-service/index.js
+++ Task-With-Microservices/task-service/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import taskRouter from './routes/task.route'
 
 dotenv.config()
 
@@ -13,3 +14,18 @@ let main = async () => {
     console.log(`Connect to Mongodb successfully`)
 }
 main().catch((err) => console.log(`Error in connecting to MongoDB`, err))
+
+app.use("/api/users", taskRouter)
+app.get("/health", (req, res) => {
+    return res.json({
+        hello: "hello task route"
+    })
+})
+
+let PORT = process.env.PORT || 3000
+app.listen(PORT, (err) => {
+    if(err) {
+        console.log("Error in creating server: ", error)
+    }
+    console.log(`Server running on: http://localhost:${PORT}/`)
+})
