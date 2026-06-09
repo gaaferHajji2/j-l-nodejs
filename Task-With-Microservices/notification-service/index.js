2c6ae3e5afca189a7e1828f3502cc1c8671efc0b
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-06-09 11:47:47 +0300

    Connect to MongoDB and create the server

--- Task-With-Microservices/notification-service/index.js
+++ Task-With-Microservices/notification-service/index.js
@@ -4,8 +4,21 @@ app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+let main = async () => {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log(`Connect to Mongodb successfully`)
+}
+main().catch((err) => console.log(`Error in connecting to MongoDB`, err))
+
 app.get("/health", (req, res) => {
     return res.json({
         hello: "hello task route"
     })
+})
+let PORT = process.env.PORT || 3001
+app.listen(PORT, (err) => {
+    if(err) {
+        console.log("Error in creating server: ", error)
+    }
+    console.log(`Server running on: http://localhost:${PORT}/`)
 })
